c82763085d298dd97b410fdee4299b3450648ea9
Joshua Barrass <josh.barrass.work@gmail.com>
2022-09-01 00:01:25 +0100

    Remove install script, as it is unused

--- install_and_cleanup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-GAMEDIR="$1"
-cd "$GAMEDIR"
-
-rm AutoRun.exe AutoRun.inf linux_installer.sh Setup.exe
-find -type f -name "*.uz2" -exec "$GAMEDIR/System/ucc-bin" decompress {} \&\& rm {} \;
